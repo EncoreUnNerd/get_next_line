8955cf719fd5632b7f294dcf2da430e7a102c836
EncoreUnNerd <mathys.n1@protonmail.com>
2024-11-01 15:58:43 +0100

    test setup

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhenin <mhenin@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 11:32:09 by mhenin            #+#    #+#             */
+/*   Updated: 2024/10/31 11:32:10 by mhenin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE = 5;
+#endif
+
+struct stock
+{
+	char *value;
+	struct stock *next;
+};
+
+#endif
