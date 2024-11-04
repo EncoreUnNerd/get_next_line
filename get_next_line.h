4d38477518f9bb825bb1b0ea339ea1c966e97937
Mathys Hénin <mathyshenin@Mathyss-MacBook-Air.local>
2024-11-04 18:20:49 +0100

    restart

--- get_next_line.h
+++ get_next_line.h
@@ -9,27 +9,3 @@
 /*   Updated: 2024/10/31 11:32:10 by mhenin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
-
-# include <stdlib.h>
-# include <unistd.h>
-
-struct s_stock
-{
-	char			value[BUFFER_SIZE];
-	struct s_stock	*next;
-};
-
-char	*get_next_line(int fd);
-char	*gett_line(struct s_stock *head);
-void	clean_to_endline(struct s_stock **head);
-int		check_if_endline(char *str);
-void	add_back(char *value, struct s_stock **head);
-
-#endif
