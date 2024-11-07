a3c9c69cfdd09d50203e8afb133f9d9537bd8808
EncoreUnNerd <mathys.n1@protonmail.com>
2024-11-07 12:14:58 +0100

    test

--- get_next_line.c
+++ get_next_line.c
@@ -9,3 +9,63 @@
 /*   Updated: 2024/10/31 11:31:29 by mhenin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	ft_is_endline(const char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE + 1];
+	int			rd_bytes;
+	char		*res;
+	static char	*stock;
+
+	if (fd < 0 || BUFFER_SIZE < 0 || read(fd, buffer, 0) < 0)
+		return (NULL);
+	rd_bytes = 1;
+	while (rd_bytes > 0)
+	{
+		rd_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (rd_bytes < 0)
+		{
+			free(stock);
+			return(NULL);
+		}
+		buffer[rd_bytes] = '\0';
+		stock = ft_strjoin(stock, buffer);
+		if (ft_is_endline(stock))
+			break ;
+	}
+	res = ft_get_line(stock);
+	stock = clear(stock);
+	return (res);
+}
+
+int	main(void)
+{
+	char	*te;
+	int		fd;
+
+	fd = open("exemple.txt", O_RDONLY);
+	te = get_next_line(fd);
+	while (te != NULL)
+	{
+		printf("%s", te);
+		te = get_next_line(fd);
+	}
+}
--- get_next_line.h
+++ get_next_line.h
@@ -9,3 +9,22 @@
 /*   Updated: 2024/10/31 11:32:10 by mhenin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, const char *s2);
+char	*clear(char *str);
+char	*ft_get_line(const char	*str);
+char	*get_next_line(int fd);
+
+#endif
