2211418e410041e26f496fa046cf60c093a678c6
EncoreUnNerd <mathys.n1@protonmail.com>
2024-11-14 17:43:42 +0100

    restart

--- get_next_line.h
+++ get_next_line.h
@@ -14,17 +14,12 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10000000
+#  define BUFFER_SIZE 10
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char *s1, const char *s2);
-char	*clear(char *str);
-char	*ft_get_line(const char	*str);
 char	*get_next_line(int fd);
 
 #endif
